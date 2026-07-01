c67dd53b0c0adbd5b868c348da049fd82b80a088
Ken Kato <kkato149@gmail.com>
2026-07-01 10:20:06 +0900

    Implement on my own

--- 1.two_sum.py
+++ 1.two_sum.py
@@ -10,9 +10,9 @@ class Solution:
   def twoSumFast(self, nums: List[int], target: int) -> List[int]:
     seen = {}
     for i, num in enumerate(nums):
-      complement = target - num
-      if complement in seen:
-        return [seen[complement], i]
+      diff = target - num
+      if diff in seen:
+        return [seen[diff], i]
       seen[num] = i
 
 
