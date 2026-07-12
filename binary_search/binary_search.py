0c1e45f51eb2306cc32eae09a17e08035656c590
Ken Kato <kkato149@gmail.com>
2026-07-12 11:17:53 +0900

    Solve Binary Search

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -1,16 +1,18 @@
 class Solution:
-  def search(self, nums: List[int], target: int) -> int:
+  def search(self, nums: list[int], target: int) -> int:
     l, r = 0, len(nums) - 1
 
     while l <= r:
       m = (l + r) // 2
       if nums[m] > target:
         r = m -1
-      else if nums[m] < target:
+      elif nums[m] < target:
         l = m + 1
       else:
         return m
 
     return -1
 
-
+if __name__ == '__main__':
+  sol = Solution()
+  print(sol.search([-1,0,2,4,6,8], 4))
